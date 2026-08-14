9a3866447aad1a8d25d531e794eaf3dc6693f134
blake mangum <blaketmangum@gmail.com>
2026-08-14 01:45:18 -0400

    created main page and added & changed CSS alot

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  new Typed("#typed", {
+    strings: [
+      "makers.",
+      "coders.",
+      "artists.",
+      "musicians.",
+      "athletes.",
+      "designers.",
+      "thinkers.",
+      "dreamers.",
+      "writers.",
+      "everyone.", 
+      "YOU."
+    ],
+    typeSpeed: 70,
+    backSpeed: 40,
+    backDelay: 900,
+    loop: true,
+    shuffle: true
+  });
+});
